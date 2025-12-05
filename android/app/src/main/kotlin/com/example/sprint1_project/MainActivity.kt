909f1bea8b88c556fb6cfc6aafe5fdc469ef06cf
resha-ad <adhikariresha@gmail.com>
2025-12-05 14:44:51 +0545

    first commit

---
+++ android/app/src/main/kotlin/com/example/sprint1_project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sprint1_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:sprint1_project/app.dart';
+
+void main() {
+  runApp(const App());
+}
