f7a5f7152c01f34605b671d5ab23690542e1d3f0
resha-ad <adhikariresha@gmail.com>
2025-12-15 11:18:03 +0545

    updated home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:sprint1_project/widgets/category_row_widget.dart';
 import 'package:sprint1_project/widgets/home_header_widget.dart';
+import 'package:sprint1_project/widgets/offer_card_widget.dart';
+import 'package:sprint1_project/widgets/product_card_widget.dart';
 import 'package:sprint1_project/widgets/search_bar_widget.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,8 +11,28 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(children: const [HomeHeader(), SearchBarWidget()]),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            HomeHeader(),
+            SearchBarWidget(),
+            CategoryRow(),
+            OfferCard(),
+
+            ProductCard(
+              title: "Pink Bouquet",
+              price: "Rs. 1700",
+              imagePath: "assets/images/bouquet.png",
+            ),
+            ProductCard(
+              title: "Tulip",
+              price: "Rs. 1200",
+              imagePath: "assets/images/tulip.png",
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
