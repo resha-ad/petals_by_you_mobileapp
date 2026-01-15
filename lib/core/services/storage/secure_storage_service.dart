0d6300349fb3dcceeab63cf2be0e96a269ff7674
resha-ad <adhikariresha@gmail.com>
2026-01-16 01:52:55 +0545

    added shared preference and secure storage service

---
+++ lib/core/services/storage/secure_storage_service.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+final secureStorageProvider = Provider<SecureStorageService>((ref) {
+  return SecureStorageService();
+});
+
+class SecureStorageService {
+  final _storage = const FlutterSecureStorage();
+  static const String _tokenKey = 'auth_token';
+
+  Future<void> saveToken(String token) async {
+    await _storage.write(key: _tokenKey, value: token);
+  }
+
+  Future<String?> getToken() async {
+    return await _storage.read(key: _tokenKey);
+  }
+
+  Future<void> deleteToken() async {
+    await _storage.delete(key: _tokenKey);
+  }
+
+  Future<void> clearAll() async {
+    await _storage.deleteAll();
+  }
+}
