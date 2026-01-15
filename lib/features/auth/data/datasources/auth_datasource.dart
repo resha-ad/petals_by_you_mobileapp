ddc10b430067455057e3b6d3d347cf57aca25e34
resha-ad <adhikariresha@gmail.com>
2026-01-16 01:56:23 +0545

    added auth remote datasource

--- lib/features/auth/data/datasources/auth_datasource.dart
+++ lib/features/auth/data/datasources/auth_datasource.dart
@@ -1,9 +1,15 @@
+import 'package:sprint1_project/features/auth/data/models/auth_api_model.dart';
 import 'package:sprint1_project/features/auth/data/models/auth_hive_model.dart';
 
-abstract interface class IAuthDataSource {
-  Future<bool> register(AuthHiveModel user);
+abstract interface class IAuthLocalDataSource {
+  Future<AuthHiveModel> register(AuthHiveModel user);
   Future<AuthHiveModel?> login(String email, String password);
   Future<AuthHiveModel?> getCurrentUser();
   Future<bool> logout();
   Future<bool> isEmailExists(String email);
+}
+
+abstract interface class IAuthRemoteDataSource {
+  Future<AuthApiModel> register(AuthApiModel user);
+  Future<AuthApiModel?> login(String email, String password);
 }
