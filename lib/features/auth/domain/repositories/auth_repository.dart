8e57cf046d65f0404bb21ace9a98b06632a42f0c
resha-ad <adhikariresha@gmail.com>
2026-01-31 17:21:24 +0545

    added update user and upload profile picture in repository

--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:dartz/dartz.dart';
 import 'package:sprint1_project/core/error/failures.dart';
 import 'package:sprint1_project/features/auth/domain/entities/auth_entity.dart';
@@ -7,4 +9,10 @@ abstract interface class IAuthRepository {
   Future<Either<Failure, AuthEntity>> login(String email, String password);
   Future<Either<Failure, AuthEntity>> getCurrentUser();
   Future<Either<Failure, bool>> logout();
+  Future<Either<Failure, String>> uploadProfilePicture(File image);
+  Future<Either<Failure, AuthEntity>> updateUser(
+    String id,
+    Map<String, dynamic> data,
+    File? image,
+  );
 }
