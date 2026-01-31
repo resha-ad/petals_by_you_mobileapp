af91f3c992f12d3f905053fab6b4843f47f77d53
resha-ad <adhikariresha@gmail.com>
2026-01-31 17:18:26 +0545

    added update user and upload profile picture in datasource

--- lib/features/auth/data/datasources/auth_datasource.dart
+++ lib/features/auth/data/datasources/auth_datasource.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:sprint1_project/features/auth/data/models/auth_api_model.dart';
 import 'package:sprint1_project/features/auth/data/models/auth_hive_model.dart';
 
@@ -12,4 +14,11 @@ abstract interface class IAuthLocalDataSource {
 abstract interface class IAuthRemoteDataSource {
   Future<AuthApiModel> register(AuthApiModel user);
   Future<AuthApiModel?> login(String email, String password);
+  Future<AuthApiModel> getUser(String id);
+  Future<AuthApiModel> updateUser(
+    String id,
+    Map<String, dynamic> data,
+    File? image,
+  );
+  Future<String> uploadProfilePicture(File image);
 }
