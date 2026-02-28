6e9ce543115e46affd76734e3a6faa8d7122f3b4
resha-ad <adhikariresha@gmail.com>
2026-02-28 14:56:55 +0545

    updated auth datasource interface

--- lib/features/auth/data/datasources/auth_datasource.dart
+++ lib/features/auth/data/datasources/auth_datasource.dart
@@ -4,21 +4,17 @@ import 'package:sprint1_project/features/auth/data/models/auth_api_model.dart';
 import 'package:sprint1_project/features/auth/data/models/auth_hive_model.dart';
 
 abstract interface class IAuthLocalDataSource {
-  Future<AuthHiveModel> register(AuthHiveModel user);
-  Future<AuthHiveModel?> login(String email, String password);
-  Future<AuthHiveModel?> getCurrentUser();
-  Future<bool> logout();
+  Future<AuthHiveModel> saveUser(AuthHiveModel user);
+  Future<AuthHiveModel?> getCachedUser();
+  Future<bool> clearUser();
   Future<bool> isEmailExists(String email);
 }
 
 abstract interface class IAuthRemoteDataSource {
-  Future<AuthApiModel> register(AuthApiModel user);
-  Future<AuthApiModel?> login(String email, String password);
-  Future<AuthApiModel> getUser(String id);
-  Future<AuthApiModel> updateUser(
-    String id,
-    Map<String, dynamic> data,
-    File? image,
-  );
-  Future<String> uploadProfilePicture(File image);
+  Future<AuthApiModel> register(AuthApiModel user, String confirmPassword);
+  Future<AuthApiModel> login(String email, String password);
+  Future<AuthApiModel> whoAmI();
+  Future<AuthApiModel> updateProfile(Map<String, dynamic> data, File? image);
+  Future<bool> forgotPassword(String email);
+  Future<bool> resetPassword(String token, String newPassword);
 }
