3d8dffadeb1a50907112a106d7ec20788c4cdf09
resha-ad <adhikariresha@gmail.com>
2026-02-28 14:56:54 +0545

    regenerated hive adapter

--- lib/features/auth/data/models/auth_hive_model.g.dart
+++ lib/features/auth/data/models/auth_hive_model.g.dart
@@ -18,42 +18,39 @@ class AuthHiveModelAdapter extends TypeAdapter<AuthHiveModel> {
     };
     return AuthHiveModel(
       authId: fields[0] as String?,
-      fullName: fields[1] as String,
-      email: fields[2] as String,
-      username: fields[3] as String?,
-      password: fields[4] as String?,
-      phoneNumber: fields[5] as String?,
-      profilePicture: fields[6] as String?,
-      address: fields[7] as String?,
-      dateOfBirth: fields[8] as String?,
-      preferredDeliveryTime: fields[9] as String?,
+      firstName: fields[1] as String,
+      lastName: fields[2] as String,
+      email: fields[3] as String,
+      username: fields[4] as String,
+      password: fields[5] as String?,
+      phone: fields[6] as String?,
+      imageUrl: fields[7] as String?,
+      role: fields[8] as String,
     );
   }
 
   @override
   void write(BinaryWriter writer, AuthHiveModel obj) {
     writer
-      ..writeByte(10)
+      ..writeByte(9)
       ..writeByte(0)
       ..write(obj.authId)
       ..writeByte(1)
-      ..write(obj.fullName)
+      ..write(obj.firstName)
       ..writeByte(2)
-      ..write(obj.email)
+      ..write(obj.lastName)
       ..writeByte(3)
-      ..write(obj.username)
+      ..write(obj.email)
       ..writeByte(4)
-      ..write(obj.password)
+      ..write(obj.username)
       ..writeByte(5)
-      ..write(obj.phoneNumber)
+      ..write(obj.password)
       ..writeByte(6)
-      ..write(obj.profilePicture)
+      ..write(obj.phone)
       ..writeByte(7)
-      ..write(obj.address)
+      ..write(obj.imageUrl)
       ..writeByte(8)
-      ..write(obj.dateOfBirth)
-      ..writeByte(9)
-      ..write(obj.preferredDeliveryTime);
+      ..write(obj.role);
   }
 
   @override
