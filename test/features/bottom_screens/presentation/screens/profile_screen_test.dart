e109fba10a005bd1ecb41725f7658f5ee8621030
resha-ad <adhikariresha@gmail.com>
2026-02-28 15:00:08 +0545

    fixed imports

--- test/features/bottom_screens/presentation/screens/profile_screen_test.dart
+++ test/features/bottom_screens/presentation/screens/profile_screen_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:sprint1_project/features/auth/domain/entities/auth_entity.dart';
 import 'package:sprint1_project/features/auth/presentation/state/auth_state.dart';
 import 'package:sprint1_project/features/auth/presentation/view_model/auth_view_model.dart';
 import 'package:sprint1_project/features/bottom_screens/presentation/screens/profile_screen.dart';
