236e9d4a0033bc72dc1a3b32e5f28c9428edeb28
resha-ad <adhikariresha@gmail.com>
2026-03-03 00:06:47 +0545

    updated main configuration

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sprint1_project/core/constants/hive_table_constants.dart';
 import 'package:sprint1_project/features/auth/data/models/auth_hive_model.dart';
+import 'package:sprint1_project/features/items/data/models/item_hive_model.dart'; // NEW
 import 'package:sprint1_project/app/app.dart';
 
 void main() async {
@@ -17,10 +18,15 @@ void main() async {
   if (!Hive.isAdapterRegistered(HiveTableConstant.authTypeId)) {
     Hive.registerAdapter(AuthHiveModelAdapter());
   }
+  // NEW — item adapter
+  if (!Hive.isAdapterRegistered(HiveTableConstant.itemTypeId)) {
+    Hive.registerAdapter(ItemHiveModelAdapter());
+  }
 
   // Open required boxes
   await Hive.openBox<AuthHiveModel>(HiveTableConstant.authTable);
   await Hive.openBox(HiveTableConstant.appSettingsTable);
+  await Hive.openBox<ItemHiveModel>(HiveTableConstant.itemTable); // NEW
 
   runApp(const ProviderScope(child: MyApp()));
 }
