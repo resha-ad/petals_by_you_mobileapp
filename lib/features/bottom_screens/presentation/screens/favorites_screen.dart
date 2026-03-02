355a4fe1f0041f4e0528facd284fc3810478bf1e
resha-ad <adhikariresha@gmail.com>
2026-03-03 01:18:28 +0545

    updated bottom navigation favorites screen

--- lib/features/bottom_screens/presentation/screens/favorites_screen.dart
+++ lib/features/bottom_screens/presentation/screens/favorites_screen.dart
@@ -1,10 +1,1 @@
-import 'package:flutter/material.dart';
-
-class FavoritesScreen extends StatelessWidget {
-  const FavoritesScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(child: Text("Favorites Screen"));
-  }
-}
+export 'package:sprint1_project/features/favorites/presentation/screen/favorites_screen.dart';
